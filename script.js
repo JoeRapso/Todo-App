097ba259406e9033ff76471b1dbf567d9a31fbf1
JoeRapso <joe.rapso@gmail.com>
2021-04-09 10:51:55 +0300

    adding a todo to dom works

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let todoCounter = 0
+
 function newTodo() {
-  alert('New TODO button clicked!')
+
+  let todoprompt = prompt('add new todo')
+  let todo = String(todoprompt)
+  let createLiElement = document.createElement('LI')
+  let liElementTextNode = document.createTextNode(todo)
+  createLiElement.appendChild(liElementTextNode)
+  list.appendChild(createLiElement)
+  todoCounter = todoCounter + 1
+  //console.log(todoCounter)
+  let createSpanElement = document.createElement('span')
+  let SpanElementTextNode = document.createTextNode(todoCounter)
+  createSpanElement.appendChild(SpanElementTextNode)
+  itemCountSpan.innerHTML= ''
+  itemCountSpan.appendChild(createSpanElement)
+
 }
